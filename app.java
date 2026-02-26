16cd1cd3a8cb9a1dd19c65009fd224b829c7dcf6
sanghavig04-dot <sanghavig04@gmail.com>
2026-02-26 13:01:37 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,6 +1,17 @@
-public class Addition {
-   public static void mian(String[]args){
-    System.out.println("A+B");
-   }
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
 }
-       
