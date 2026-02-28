c07111951b6668b6ebc005398ba3e66c7fe3b9eb
sanghavig04-dot <sanghavig04@gmail.com>
2026-02-28 12:02:57 +0530

    Update app.java

--- app.java
+++ app.java
@@ -9,7 +9,7 @@ public class app {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
 
-        int sum = a + b;
+        int sum = a - b;
 
         System.out.println("Sum = " + sum);
     }
